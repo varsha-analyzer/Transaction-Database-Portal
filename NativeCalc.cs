94bb00a46d0d18c99e578d8e730cd23ea04b35d2
Varsha M <varshajaimohan@gmail.com>
2026-07-28 18:00:24 +0530

    Update NativeCalc.cs

--- NativeCalc.cs
+++ NativeCalc.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 class NativeCalc
 {
     [DllImport("calc", CallingConvention = CallingConvention.Cdecl)]
-    public static extern double CalculateTotal(double price, int quantity);
+    public static extern double CalculateTotal(double price, int quantity;
 
     public static double GetTotal(double price, int quantity)
     {
